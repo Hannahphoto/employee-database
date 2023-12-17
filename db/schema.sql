801f847e34e6a82b0f5484cc091a96868877f2b8
Hannahphoto <hannahprice67@gmail.com>
2023-12-17 16:14:27 -0500

    organized seeds better

--- db/schema.sql
+++ db/schema.sql
@@ -4,24 +4,25 @@ CREATE DATABASE college_db;
 USE college_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO-INCREMENT,
-    department_name VARCHAR(30);
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(30)
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO-INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     role_title VARCHAR(30),
-    role_salary DECIMAL NULL
+    role_salary DECIMAL,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
-)
+);
 
-CREAT TABLE employee (
-    id INT NOT NULL PRIMARY KEY AUTO-INCREMENT,
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
+    PRIMARY KEY (id),
 
     FOREIGN KEY (role_id)
     REFERENCES role(id),
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,32 +1,27 @@
 INSERT INTO department (id, department_name)
-VALUES  (01, "Art"),
-        (02, "English"),
-        (03, "Math"),
-        (04, "Civics & Government"),
-        (05, "Media"),
-        (06, "Science"),
-        (07, "Accounting"),
-        (08, "Economics"),
-        (09, "Computer Science"),
-        (10, "History");
+VALUES  (01, "Pre-Production"),
+        (02, "Production"),
+        (03, "Post-Production"),
+        (04, "Marketing");
+        
 
 INSERT INTO role (id, role_title, role_salary, department_id)
-VALUES  (01, "Department Head/Manager", $100,000.00),
-        (02, "Professor", $80,000.00),
-        (03, "Instructor", $70,000.00),
-        (04, "Department Specialist", $60,000.00),
-        (05, "Department Staff", $55,000.00),
-        (06. "Work Study", $40,000.00);
+VALUES  (01, "Producer", $100,000.00, 02),
+        (02, "Writer", $80,000.00, 01),
+        (03, "Editor", $75,000.00, 03),
+        (04, "Promoter", $80,000.00, 04),
+        (05, "Assistant Producer", $70,000.00, 02),
+        (06. "Actor/Talent", $90,000.00, 02);
 
-INSERT INTO employee (id, first_name, last_name, role_id, department_id,)
-VALUES  (01, "Sabrina", "Salvati", "Department Head/Mangaer", 05),
-        (02, "Richard", "Wolf", "Professor", 08),
-        (03, "Glenn", "Greenwald", "Instructor", 04),
-        (04, "Alif", "Vasquez", "Department Specialist", 09),
-        (05, "Hannah", "Price", "Department Staff", 07),
-        (06, "Aeryn", "Daboin", "Work Study", 06),
-        (07, "Marta", "Mack", "Department Head/Mangaer", 10),
-        (08, "Mike", "Mansen", "Professor", 01),
-        (09, "Sarah","Johnson", "Instructor", 02),
-        (10, "Denali", "Smith", "Instructor", 03);
+INSERT INTO employee (id, first_name, last_name, role_title, department_id,)
+VALUES  (01, "Sabrina", "Salvati", "Actor/Talent", 02),
+        (02, "Richard", "Wolf", "Producer", 02),
+        (03, "Glenn", "Greenwald", "Writer", 01),
+        (04, "Alif", "Vasquez", "Ediotr", 03),
+        (05, "Hannah", "Price", "Promoter", 04),
+        (06, "Aeryn", "Daboin", "Assistant Producer", 02),
+        (07, "Marta", "Mack", "Actor/Talent", 02),
+        (08, "Mike", "Mansen", "Producer", 02),
+        (09, "Sarah","Johnson", "Writer", 01),
+     
 
